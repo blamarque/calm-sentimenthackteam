5f0be59bf9ab1bc83b28f064e1b588122aa0b99c
Bertrand Lamarque <bertrand@brainbowgames.com>
2018-02-24 12:30:57 +0000

    Adding en

---
+++ wordAnalys.js
@@ -0,0 +1,13 @@
+var S3Reader = require(__base +'S3Reader');
+var env = require(__base +'env');
+
+var variationBuilder = require(__base +'Variation');
+
+(function () {
+
+	var wordAnalysis = {};
+
+
+	module.exports = wordAnalysis;
+
+} ());
