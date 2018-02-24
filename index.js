7dee4abac2da193e3f2b6c627ae9482ac39e83f8
Bertrand Lamarque <bertrand@brainbowgames.com>
2018-02-24 15:42:00 +0000

    fix

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ var sentiment = require('./sentiment-api.js');
 var configurationReader = require(__base + 'RessourceConfigurationReader');
 var ruleService = require(__base + 'Rules');
 
-console.log("Hello World");
-//console.log(sentiment);
-var ressourcesConfiguration = configurationReader.read({}, function(ressourcesConfiguration){
 
-    console.log("Hello World " + JSON.stringify(ressourcesConfiguration));
-    if(ressourcesConfiguration[0].rules.openingTimeRule[0].apply("now")) {
 
-    }
-});
 
+
+module.exports = function (number, locale) {
+    console.log("Hello World");
+    //console.log(sentiment);
+    var ressourcesConfiguration = configurationReader.read({}, function (ressourcesConfiguration) {
+
+        console.log("Hello World " + JSON.stringify(ressourcesConfiguration));
+        if (ressourcesConfiguration[0].rules.openingTimeRule[0].apply("now")) {
+
+        }
+    });
+
+    return {};
+};
 
 
 
